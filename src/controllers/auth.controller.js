f635bb63c73d4d766f595c439a120e4569b9990f
Arizidan <samionohari@gmail.com>
2024-10-10 20:57:02 +0700

    fix: login & register controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -6,20 +6,20 @@ async function register(request, reply) {
     const { username, password } = request.body;
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    const [newAdmin] = await db('admin').insert({ username, password: hashedPassword }).returning('*');
+    const [newUsers] = await db('users').insert({ username, password: hashedPassword }).returning('*');
 
-    reply.code(201).send({ message: 'Admin registered successfully', admin: newAdmin });
+    reply.code(201).send({ message: 'Users registered successfully', users: newUsers });
 }
 
 async function login(request, reply) {
     const { username, password } = request.body;
-    const admin = await db('admin').where({ username }).first();
+    const users = await db('users').where({ username }).first();
 
-    if (!admin || !(await bcrypt.compare(password, admin.password))) {
+    if (!users || !(await bcrypt.compare(password, users.password))) {
         return reply.code(401).send({ message: 'Invalid credentials' });
     }
 
-    const token = jwt.sign({ id: admin.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    const token = jwt.sign({ id: users.id }, process.env.JWT_SECRET, { expiresIn: '1h' });
     reply.send({ token });
 }
 
