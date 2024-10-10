cb86b80f743f8f5ffb4b772503190bcae332f330
Arizidan <samionohari@gmail.com>
2024-10-10 20:56:10 +0700

    feat: CORS & JWT plugin register

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 const fastify = require('fastify')({ logger: true });
 const dotenv = require('dotenv');
+const fastifyJwt = require('@fastify/jwt');
+const cors = require('@fastify/cors');
 
 dotenv.config();
 
+// Register CORS plugin
+fastify.register(cors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+});
+
+// plugin JWT
+fastify.register(fastifyJwt, { secret: process.env.JWT_SECRET });
+
+// Middleware auth
+fastify.decorate('authenticate', async (request, reply) => {
+    try {
+        await request.jwtVerify();
+    } catch (err) {
+        reply.send(err);
+    }
+});
+
+// route
 const itemRoutes = require('./routes/item.routes');
 const authRoutes = require('./routes/auth.routes');
 
