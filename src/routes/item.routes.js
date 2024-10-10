7b6af34a8752d669ce4c8cc79f18aca0cb6a9c14
Arizidan <samionohari@gmail.com>
2024-10-10 17:13:05 +0700

    feat: item routes

---
+++ src/routes/item.routes.js
@@ -0,0 +1,12 @@
+// src/routes/itemRoutes.js
+const itemController = require('../controllers/item.controller');
+const authMiddleware = require('../middlewares/auth.middleware');
+
+module.exports = function (fastify, opts, done) {
+  fastify.post('/items', { preHandler: [authMiddleware] }, itemController.createItem);
+  fastify.put('/items/:id', { preHandler: [authMiddleware] }, itemController.updateItem);
+  fastify.post('/items/:id/reduce-stock', { preHandler: [authMiddleware] }, itemController.reduceStock);
+  fastify.get('/inventory-value', { preHandler: [authMiddleware] }, itemController.getAllItems);
+
+  done();
+};
