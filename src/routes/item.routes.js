b461e5d13e185354ebfd4743f738488eb744fce1
Arizidan <samionohari@gmail.com>
2024-10-10 21:07:06 +0700

    fix: items endpoint

--- src/routes/item.routes.js
+++ src/routes/item.routes.js
@@ -2,10 +2,10 @@ const itemController = require('../controllers/item.controller');
 const authMiddleware = require('../middlewares/auth.middleware');
 
 module.exports = function (fastify, opts, done) {
-  fastify.post('/items', { preHandler: [authMiddleware] }, itemController.createItem);
-  fastify.put('/items/:id', { preHandler: [authMiddleware] }, itemController.updateItem);
-  fastify.post('/items/:id/reduce-stock', { preHandler: [authMiddleware] }, itemController.reduceStock);
-  fastify.get('/inventory-value', { preHandler: [authMiddleware] }, itemController.getAllItems);
+    fastify.post('/', { preHandler: [authMiddleware] }, itemController.createItem);
+    fastify.put('/:id', { preHandler: [authMiddleware] }, itemController.updateItem);
+    fastify.post('/:id/reduce-stock', { preHandler: [authMiddleware] }, itemController.reduceStock);
+    fastify.get('/', { preHandler: [authMiddleware] }, itemController.getAllItems);
 
-  done();
+    done();
 };
